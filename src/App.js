75d68de0de4f8c172202df6ecb3adb090d0fa99c
Arman <aisenovdev@gmail.com>
2023-03-31 13:34:12 +0500

    layout created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [service, setService] = useState("");
+  const [person, setPerson] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <h2>Оплата</h2>
+        <label htmlFor="person">ФИО:</label>
+        <input
+          type="text"
+          id="person"
+          value={person}
+          onChange={(e) => setPerson(e.target.value)}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="tel">Телефон:</label>
+        <input
+          type="number"
+          id="tel"
+          value={tel}
+          onChange={(e) => setTel(e.target.value)}
+        />
+        <select value={service} onChange={(e) => setService(e.target.value)}>
+          <option disabled value="">
+            Выберите услугу
+          </option>
+          <option value="рабочееместо">Рабочее место</option>
+          <option value="конференцзал">Конференц-зал</option>
+          <option value="переговорка">Переговорка</option>
+        </select>
+        <input type="submit" id="post" value="Отправить" />
+      </form>
     </div>
   );
 }
